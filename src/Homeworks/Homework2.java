cbff3e24fb2682266d862bf43c217743da6ea4a5
gayvarosyan <aggss.1881@gmail.com>
2025-09-13 00:28:44 +0400

    my second homework

---
+++ src/Homeworks/Homework2.java
@@ -0,0 +1,67 @@
+package Homeworks;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        // exercise 1
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > j; j--) {
+                System.out.print("*");
+            }
+            for (int k = 0; k <= i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        //exercise 2
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //exercise 3
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        //exercise 4
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //exercise5
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 3; i >= 1; i--) {
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
