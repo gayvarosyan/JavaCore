8ccf2dbdcae5d0126ccbe7467403aca091b394d7
gayvarosyan <aggss.1881@gmail.com>
2025-09-19 19:50:33 +0400

    examples

---
+++ src/Classworks/While.java
@@ -0,0 +1,11 @@
+package Classworks;
+
+public class While {
+    public static void main(String[] args) {
+        int n = 10;
+        while (n > 0) {
+            System.out.println( "тaкт" + n);
+            n--;
+        }
+    }
+}
