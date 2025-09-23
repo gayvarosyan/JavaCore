3ba258ffa3de4e4fd6d79364eecc7e62b8e2a7b2
gayvarosyan <aggss.1881@gmail.com>
2025-09-23 17:33:06 +0400

    SortHomework

--- src/Homeworks/Homework5.java
+++ src/Homeworks/Homework5.java
@@ -1,13 +1,26 @@
 package Homeworks;
 
-import java.util.Arrays;
 
 public class Homework5 {
     public static void main(String[] args) {
-        int [] array = {4,7,1,3,9,0,2};
-        Arrays.sort(array);
-        for(int num: array) {
+        int[] numbers = {3, 4, 16, 74, 9, 6, 2, 22, 11, 33};
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] < numbers[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = numbers[i];
+            numbers[i] = numbers[minIndex];
+            numbers[minIndex] = temp;
+        }
+
+        System.out.println("Array sorted in ascending order:");
+        for (int num : numbers) {
             System.out.print(num + " ");
         }
     }
 }
+
