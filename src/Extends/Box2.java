9b20cec8a44b7e658c19e72489f3e291a250bb1e
gayvarosyan <aggss.1881@gmail.com>
2025-10-13 19:01:10 +0400

    classwork

---
+++ src/Extends/Box2.java
@@ -0,0 +1,30 @@
+package Extends;
+
+public class Box2 {
+    double width;
+    double height;
+    double depth;
+
+    Box2(BoxWeight2 ob) {
+        width = ob.width;
+        height = ob.height;
+        depth = ob.depth;
+    }
+
+    Box2(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+     Box2() {
+        width = -1;
+        height = -1;
+        depth = -1;
+     }
+     Box2 (double len) {
+        width = height = depth = len;
+     }
+     double volume() {
+        return width * height * depth;
+     }
+}
