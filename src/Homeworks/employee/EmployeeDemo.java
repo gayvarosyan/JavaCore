3a6225407f0fc07400ac897a298af689773aa5d5
gayvarosyan <aggss.1881@gmail.com>
2025-11-01 22:31:01 +0400

    Homework

--- src/Homeworks/employee/EmployeeDemo.java
+++ src/Homeworks/employee/EmployeeDemo.java
@@ -4,76 +4,102 @@ import java.util.Scanner;
 
 public class EmployeeDemo {
 
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final EmployeeStorage storage = new EmployeeStorage();
+    private static final int EXIT = 0;
+    private static final int  ADD_EMPLOYEE = 1;
+    private static final int PRINT_ALL_EMPLOYEES = 2;
+    private static final int SEARCH_EMPLOYEE_BY_ID = 3;
+    private static final int SEARCH_EMPLOYEE_BY_COMPANY_NAME = 4;
+
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        EmployeeStorage storage = new EmployeeStorage();
 
         boolean isRunning = true;
 
         while (isRunning) {
-            System.out.println("""
-                    Please choose an option:
-                    0 - Exit
-                    1 - Add employee
-                    2 - Print all employees
-                    3 - Search employee by ID
-                    4 - Search employee by company name
-                    """);
-
+            printCommands();
             int command = Integer.parseInt(scanner.nextLine());
 
             switch (command) {
                 case 0:
-                    isRunning = false;
                     System.out.println("Program ended.");
+                    isRunning = false;
                     break;
-                case 1:
-                    System.out.print("Enter name: ");
-                    String name = scanner.nextLine();
-                    System.out.print("Enter surname: ");
-                    String surname = scanner.nextLine();
-                    System.out.print("Enter employee ID (e.g., A0001): ");
-                    String id = scanner.nextLine();
-                    System.out.print("Enter salary: ");
-                    double salary = Double.parseDouble(scanner.nextLine());
-                    System.out.print("Enter company: ");
-                    String company = scanner.nextLine();
-                    System.out.print("Enter position: ");
-                    String position = scanner.nextLine();
 
-                    Employee emp = new Employee(name, surname, id, salary, company, position);
-                    storage.add(emp);
-                    System.out.println("Employee added successfully!\n");
+                case 1:
+                    addEmployee();
                     break;
 
                 case 2:
-                    storage.printAll();
+                    printAllEmployees();
                     break;
 
                 case 3:
-                    System.out.print("Enter employee ID: ");
-                    String searchId = scanner.nextLine();
-                    Employee found = storage.getById(searchId);
-                    if (found != null) {
-                        System.out.println(found);
-                    } else {
-                        System.out.println("Employee not found with ID: " + searchId);
-                    }
+                    searchById();
                     break;
 
                 case 4:
-                    System.out.print("Enter company name: ");
-                    String companyName = scanner.nextLine();
-                    storage.searchByCompany(companyName);
+                    searchByCompany();
                     break;
 
                 default:
                     System.out.println("Invalid command. Try again.");
             }
         }
+
+        scanner.close();
+    }
+
+    private static void printCommands() {
+        System.out.println("Please input" + EXIT + "for EXIT");
+        System.out.println("Please input " + ADD_EMPLOYEE + " for ADD EMPLOYEE");
+        System.out.println("Please input " + PRINT_ALL_EMPLOYEES + " for PRINT ALL EMPLOYEES");
+        System.out.println("Please input " + SEARCH_EMPLOYEE_BY_ID + " for SEARCH EMPLOYEE BY ID");
+        System.out.println("Please input " + SEARCH_EMPLOYEE_BY_COMPANY_NAME + " for SEARCH EMPLOYEE BY COMPANY NAME");
+    }
+
+    private static void addEmployee() {
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter surname: ");
+        String surname = scanner.nextLine();
+        System.out.print("Enter employee ID (e.g., A0001): ");
+        String id = scanner.nextLine();
+        System.out.print("Enter salary: ");
+        double salary = Double.parseDouble(scanner.nextLine());
+        System.out.print("Enter company: ");
+        String company = scanner.nextLine();
+        System.out.print("Enter position: ");
+        String position = scanner.nextLine();
+
+        Employee emp = new Employee(name, surname, id, salary, company, position);
+        storage.add(emp);
+        System.out.println("Employee added successfully!");
+    }
+
+    private static void printAllEmployees() {
+        System.out.println("All Employees:");
+        storage.printAll();
+        System.out.println();
+    }
+
+    private static void searchById() {
+        System.out.print("Enter employee ID: ");
+        String searchId = scanner.nextLine();
+        Employee found = storage.getById(searchId);
+        if (found != null) {
+            System.out.println(found);
+        } else {
+            System.out.println("Employee not found with ID: " + searchId);
+        }
+        System.out.println();
+    }
+
+    private static void searchByCompany() {
+        System.out.print("Enter company name: ");
+        String companyName = scanner.nextLine();
+        storage.searchByCompany(companyName);
+        System.out.println();
     }
 }
-
-
-
